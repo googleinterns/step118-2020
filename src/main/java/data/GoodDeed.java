f3a0a5a295353b2cfce1d344cc0c1abc34d99b62
Sergio Ortiz <ortizsergio1234@gmail.com>
2020-06-22 23:03:27 +0000

    sergio test
    test commit
    
    Add Eric to README as a test
    
    Add last name to Eric for second test commit
    
    Remove last name for Eric as a 3rd test commit
    
    Add structural elements
    
    This commit adds the necessary structural elements such as basic HTML, CSS, JavaScript to allow work to begin on the MVP. Previously, this repo was practically entirely empty and only had a README and a .txt file.
    
    Basic Framework for Deed Database
    
    testing commits
    
    Tests commits on branches
    
    Fixes styling by indenting by two spaces at a time
    
    Resolve README merge conflict
    
    Add .gitignore file to ignore auto-generated target/ folder
    
    Previously, there was no .gitignore file and Git tracked files that are not intended to be in the repo. This commit changes that by untracking those files and adding a .gitignore
    
    Abstract strings and make Good Deed attributes final
    
    Adds styling choices for headers and paragraphs
    
    Adds header and simple intro paragraph
    
    Change author email to eliminate cla problem
    
    Basic framework for datbase
    
    Rebase
    
    Sergio
    
    testing commits
    
    Tests commits on branches
    
    Fixes styling by indenting by two spaces at a time
    
    Resolve README merge conflict
    
    Add .gitignore file to ignore auto-generated target/ folder
    
    Previously, there was no .gitignore file and Git tracked files that are not intended to be in the repo. This commit changes that by untracking those files and adding a .gitignore
    
    Adds styling choices for headers and paragraphs
    
    Adds header and simple intro paragraph
    
    rebase
    
    Abstract strings and make Good Deed attributes final
    
    Change author email to eliminate cla problem
    
    Basic framework for datbase

---
+++ src/main/java/data/GoodDeed.java
@@ -0,0 +1,38 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+public class GoodDeed {
+    private final long id;
+    private final String title;
+    private final String description;
+    private boolean posted_yet;
+    private final long timestamp;
+
+    public GoodDeed (long id, String title, String description, boolean posted_yet, long timestamp) {
+        if (title == null) {
+            throw new IllegalArgumentException("Title cannot be null");
+        }
+
+        if (description == null) {
+            throw new IllegalArgumentException("Description cannot be null");
+        }
+        
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.posted_yet = posted_yet;
+        this.timestamp = timestamp;
+    }
+
+}
