d3e1c466cd0f2cebdea1c11639d1bbc4b1409adc
Eric <lierichaomin@gmail.com>
2020-07-01 22:34:20 +0000

    Add tests for login/logout functionality
    Previously, there were no tests for the login/logout functionality. This commit adds those tests by separating out the logic of the doGet() function in the /login servlet into another function getAuthentication(). This getAuthentication() function is tested via JUnit for correctness by creating an expected return in the LoginTest.java file.
    
    For clarification, the Login.java and Authentication.java files are moved into sps/login so that they are in the same package and can be easily accessed by the JUnit tests in LoginTest.java. Additionally, a number of necessary dependencies for appengine testing are added to the pom.xml file.

--- src/main/java/com/google/sps/login/Login.java
+++ src/main/java/com/google/sps/login/Login.java
@@ -12,19 +12,19 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.sps.servlets;
+package com.google.sps.login;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.KeyFactory;
 import com.google.gson.Gson;
-import com.google.sps.data.Authentication;
+import com.google.sps.login.Authentication;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-
 
 @WebServlet("/login")
 public class Login extends HttpServlet {
@@ -33,35 +33,34 @@ public class Login extends HttpServlet {
     private static final String REDIRECT_LOGIN = "/";
     private static final String REDIRECT_LOGOUT = "/";
     
-    // Print json of whether user is logged in and the respective login/logout link
+    // Passes in the actual userService to getAuthentication
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        Authentication auth;
-        
-        response.setContentType("application/json");
-
         UserService userService = UserServiceFactory.getUserService();
+        
+        // Get the login status and link
+        Authentication auth = getAuthentication(userService);
+
+        response.getWriter().println(new Gson().toJson(auth));
+
+    }
+
+    // Return Authentication class that includes login status and login/logout link
+    public Authentication getAuthentication(UserService userService) {
+        Authentication auth;
+
         if (userService.isUserLoggedIn()) {
             String logoutUrl = userService.createLogoutURL(REDIRECT_LOGOUT);
-            String userEmail = userService.getCurrentUser().getEmail();
 
             auth = new Authentication(true, logoutUrl);
-
-            Gson gson = new Gson();
-            String json = gson.toJson(auth);
-
-            response.getWriter().println(json);
         }
         else {
             String loginUrl = userService.createLoginURL(REDIRECT_LOGIN);
 
             auth = new Authentication(false, loginUrl);
-
-            Gson gson = new Gson();
-            String json = gson.toJson(auth);
-
-            response.getWriter().println(json);
         }
+
+        return auth;
     }
 }
