bec147f819a2d5dff9bfe85165730e32b772dbbf
Shawn Jackson <shawnjj@google.com>
2020-07-01 16:20:44 +0000

    Create Test class for Deed Object. Move input from to DeedInput html

--- src/test/java/com/google/sps/database/GoodDeedTesting.java
+++ src/test/java/com/google/sps/database/GoodDeedTesting.java
@@ -12,3 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.sps.data.GoodDeed;
+
+@RunWith(JUnit4.class)
+public final class GoodDeedTesting {
+    private static final String GOOD_DEED = "Good Deed";
+    private static final String KEY = "Key";
+    private static final long ID = 12345;
+    private static final String TITLE = "Deed Name";
+    private static final String DESCRIPTION = "Deed Description";
+    private static final boolean POSTED_YET = true;
+    private static final long TIMESTAMP = 67890;
+    
+    @Test
+    public void throwsNullName() {
+        Key k1 = KeyFactory.createKey(GOOD_DEED, KEY);
+        GoodDeed deed = new GoodDeed(k1, ID, null, DESCRIPTION, POSTED_YET, TIMESTAMP);
+        Assert.assertNull(deed);
+    }
+
+    @Test
+    public void throwsNullDescription() {
+        Key k1 = KeyFactory.createKey(GOOD_DEED, KEY);
+        GoodDeed deed = new GoodDeed(k1, ID, TITLE, null, POSTED_YET, TIMESTAMP);
+        Assert.assertNull(deed);
+    }
+
+    @Test
+    public void testGetKey() {
+        Key k1 = KeyFactory.createKey(GOOD_DEED, KEY);
+        GoodDeed deed = new GoodDeed(k1, ID, TITLE, DESCRIPTION, POSTED_YET, TIMESTAMP);
+        Key actual = deed.getKey();
+
+        Assert.assertEquals(k1, actual);
+    }
+
+    @Test
+    public void testGetPosted() {
+        Key k1 = KeyFactory.createKey(GOOD_DEED, KEY);
+        GoodDeed deed = new GoodDeed(k1, ID, TITLE, DESCRIPTION, POSTED_YET, TIMESTAMP);
+        boolean actual = deed.getPosted();
+
+        Assert.assertEquals(POSTED_YET, actual);
+    }
+}
