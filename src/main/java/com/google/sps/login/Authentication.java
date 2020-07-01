d2f0412b99b314100af089ddcc5a34c15efeeaf5
Eric <lierichaomin@gmail.com>
2020-07-01 17:38:51 +0000

    Add tests for login/logout functionality
    Previously, there were no tests for the login/logout functionality. This commit adds those tests by separating out the logic of the doGet() function in the /login servlet into another function getAuthentication(). This getAuthentication() function is tested via JUnit for correctness by creating an expected return in the LoginTest.java file.
    
    For clarification, the Login.java and Authentication.java files are moved into sps/login so that they are in the same package and can be easily accessed by the JUnit tests in LoginTest.java. Additionally, a number of necessary dependencies for appengine testing are added to the pom.xml file.

--- src/main/java/com/google/sps/login/Authentication.java
+++ src/main/java/com/google/sps/login/Authentication.java
@@ -10,9 +10,8 @@
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
-// limitations under the License.
 
-package com.google.sps.data;
+package com.google.sps.login;
 
 // Authentication class that stores whether the user is logged in and the redirect link
 public final class Authentication {
@@ -23,5 +22,13 @@ public final class Authentication {
     public Authentication(boolean loggedIn, String redirectLink) {
         this.loggedIn = loggedIn;
         this.redirectLink = redirectLink;
+    }
+
+    public boolean getLoggedIn() {
+        return loggedIn;
+    }
+
+    public String getRedirectLink() {
+        return redirectLink;
     }
 }
