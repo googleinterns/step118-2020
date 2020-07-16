5d833afff2ce53a87787edd1aa1d6850ee74a03d
Eric <lierichaomin@gmail.com>
2020-07-16 21:46:52 +0000

    Move register and login from Users API to Firebase
    Previously, 1Deed1Day implemented Users API which ran into the issue where logout would log the user out of every single account that they were logged in on. This commit changes the User API implementation to use Firebase instead. Key implementation changes include adding Google single-sign on, removing Datastores from authentication features, and complete rewrite of the tests.

---
+++ src/main/webapp/script-login.js
@@ -0,0 +1,83 @@
+// initialize the firebase and the UI
+function onLoad() {
+    authInitializeFirebase();
+    checkLoginWithUI();
+}
+
+// check if user is logged in and show buttons accordingly
+function checkLoginWithUI() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+            showLogoutNoUI();
+        }
+        else {
+            showLoginAndUI();
+        }
+    });
+}
+
+// show login button with UI, because user is not logged in
+function showLogoutNoUI() {
+    document.getElementById('loginBtn').style.display = 'none';
+    document.getElementById('logoutBtn').style.display = 'block';
+
+    document.getElementById('disclaimer').style.display = 'none';
+
+    // if logged in, display this message
+    document.getElementById('firebaseui-auth-container').innerHTML = 'You have already logged in. Log out to sign in with a different account!';
+}
+
+// show logout button without UI, because user is logged in
+function showLoginAndUI() {
+    document.getElementById('loginBtn').style.display = 'block';
+    document.getElementById('logoutBtn').style.display = 'none';
+
+    document.getElementById('disclaimer').style.display = 'block';
+
+    // if not logged in, display login UI
+    initializeFirebaseUI();
+}
+
+// initialize drop-in UI for firebase
+function initializeFirebaseUI() {
+    //clear container
+    document.getElementById('firebaseui-auth-container').innerHTML = '';
+    
+    var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+    ui.start('#firebaseui-auth-container', {
+        signInSuccessUrl: '/',
+        signInOptions: [
+            firebase.auth.EmailAuthProvider.PROVIDER_ID,
+            firebase.auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+    });
+}
+
+/** SLIGHTLY MODIFIED FUNCTIONS FOR TESTING */
+function showLoginAndUITest(mockFn) {
+    document.getElementById('loginBtn').style.display = 'block';
+    document.getElementById('logoutBtn').style.display = 'none';
+
+    document.getElementById('disclaimer').style.display = 'block';
+
+    // if not logged in, display login UI
+    mockFn.initializeFirebaseUI();
+}
+
+function checkLoginWithUITest(mockFn) {
+    mockFn.onAuthStateChanged(function(user) {
+        if (user) {
+            mockFn.showLogoutNoUI();
+        }
+        else {
+            mockFn.showLoginAndUI();
+        }
+    });
+}
+
+module.exports = {
+    showLogoutNoUITest: showLogoutNoUI,
+    showLoginAndUITest: showLoginAndUITest,
+    checkLoginWithUITest: checkLoginWithUITest
+}
