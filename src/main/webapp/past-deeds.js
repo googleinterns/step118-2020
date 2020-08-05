fa630d7e2d92a3e3eb9fb6d8df74b119ea8b33d4
Eric <lierichaomin@gmail.com>
2020-08-05 17:00:12 +0000

    Show correct login on survey input and past deeds page.
    Previously, the survey input and past deeds pages did not include the updated authentication functioanlity. This commit adds the correct authentication functionality and makes the survey input page restricted to signed in users.

--- src/main/webapp/past-deeds.js
+++ src/main/webapp/past-deeds.js
@@ -12,6 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function onLoad() {
+    authInitializeFirebase();
+    authCheckLogin(false);
+    displayPastDeeds();
+}
+
 async function displayPastDeeds() {
     const DEED_LIST = "deed-list";
     const DEED_URL = "/ghost-of-deeds-past";
